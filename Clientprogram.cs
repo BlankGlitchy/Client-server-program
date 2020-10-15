2653791ded7647f43100f52f7e9e7fd7d2b98296
Morten Allan Jensen <mallanj@ruc.dk>
2020-10-15 12:48:21 +0200

    Server and client programs. can connect to eachother

---
+++ Clientprogram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello World!");
+            // Client connection
+            var client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 5000);
+
+            var stream = client.GetStream();
+
+            var data = Encoding.UTF8.GetBytes("Hello");
+
+            stream.Write(data);
+
+            data = new byte[client.ReceiveBufferSize];
+
+            var cnt = stream.Read(data);
+
+            var msg = Encoding.UTF8.GetString(data, 0, cnt);
+
+            Console.WriteLine("MEssage from server");
+        }
+    }
+}
